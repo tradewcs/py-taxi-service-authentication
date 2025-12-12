57ef5b234babb66c7f982516a308c0bcf05568ad
Mike <tradewcs@gmail.com>
2025-12-12 18:58:44 +0200

    -

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 from .views import (
     index,
@@ -22,6 +23,8 @@ urlpatterns = [
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
+    path('login/', RedirectView.as_view(url='accounts/login/', permanent=False)),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 app_name = "taxi"
